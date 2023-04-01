52d7ce1fe3c903c072d371a30a28e3b529609267
John Diggins <rohnfiggins@gmail.com>
2023-03-31 18:27:14 -0700

    migration after validations

--- ShelterAPI/Models/Animal.cs
+++ ShelterAPI/Models/Animal.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ShelterAPI.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+
+    [Required]
+    [StringLength(25)]
     public string Name { get; set; }
+
+    [Required]
     public string Species { get; set; }
+
     public string Gender {get; set;}
+
+    [Required]
+    [Range(0, 30, ErrorMessage = "I've never heard of a dog or cat that age.")]
     public int Age { get; set; } 
   }
 }
